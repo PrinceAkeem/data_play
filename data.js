34f97b82ae6428b007d26cf230ad93f1b943e1ac
Delmer <delmer814+1@gmail.com>
2017-03-23 17:59:02 -0400

    First

---
+++ data.js
@@ -0,0 +1,138 @@
+var data = {
+    categories: [
+        "Meat",
+        "Turkey",
+        "Chicken and Other Poultry",
+        "Seafood",
+        "Eggs and Egg Products",
+        "Milk, Cheese, and Dairy Products",
+        "Fresh Fruits, Vegetables, and Juices",
+        "Nuts, Grains, and Beans",
+        "Baby Food and Infant Formula",
+        "Pet Food"
+    ],
+    warnings: [{
+        "message": "Raw meat may contain bacteria, such as E. coli, salmonella, and listeria, or parasites. Thorough cooking destroys these harmful organisms, but meat can become contaminated again if it is not handled and stored properly.",
+        "type": "Meat"
+    }, {
+        "message": "Turkey is often associated with holidays and parties. But, turkey can also be associated with foodborne illness if it is not thawed, prepared, cooked, and stored properly.",
+        "type": "Turkey"
+    }, {
+        "message": "Poultry may contain harmful bacteria such as salmonella, listeria, and campylobacter. Washing chicken and other poultry does not remove bacteria. You can kill these bacteria only by cooking chicken to the proper temperature.",
+        "type": "Chicken and Other Poultry"
+    }, {
+        "message": "Like raw meat, raw seafood may contain bacteria that can be destroyed only by cooking. Some seafood may also contain toxins such as mercury which may be harmful for young children or an unborn baby.",
+        "type": "Seafood"
+    }, {
+        "message": "Fresh eggs must be handled carefully. Even eggs with clean, uncracked shells may occasionally contain salmonella. To prevent food poisoning, keep eggs refrigerated, cook eggs until yolks are firm, and cook foods containing eggs thoroughly.",
+        "type": "Eggs and Egg Products"
+    }, {
+        "message": "Raw milk, as well as cheeses made with raw milk, may contain E. coli, salmonella, and listeria. That’s why it’s important to make sure that milk has been pasteurized, which kills harmful bacteria.",
+        "type": "Milk, Cheese, and Dairy Products"
+    }, {
+        "message": "Fresh produce may come in contact with harmful bacteria from many sources, from contaminated soil and water in the fields to a contaminated cutting board in the kitchen. Fruit and vegetable juices must be treated to kill bacteria.",
+        "type": "Fresh Fruits, Vegetables, and Juices"
+    }, {
+        "message": "Nuts, grains, beans, and other legumes, and their by-products, are found in a wide variety of foods. Since these foods are ingredients in so many food products, contamination or mislabeling of allergens can pose a widespread risk.",
+        "type": "Nuts, Grains, and Beans"
+    }, {
+        "message": "Infants and young children are particularly vulnerable to foodborne illness because their immune systems are not developed enough to fight off infections. That's why extra care should be taken when handling and preparing their food and formula.",
+        "type": "Baby Food and Infant Formula"
+    }, {
+        "message": "Like human food, pet food may contain harmful bacteria (such as salmonella) or toxins (such as melamine). If pet food is not handled properly, both pets and humans may be at risk for foodborne illness.",
+        "type": "Pet Food"
+    }],
+    items: [{
+        "type": "Meat",
+        "name": "Beef",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…food-safety-fact-sheets/meat-preparation/beef-from-farm-to-table/ct_index\" id=\"anch_83\">Beef: From Farm to Table</a> (USDA)<br>Includes charts on beef cooking times and storage times.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Bison",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-answers/food-safety-fact-sheets/meat-preparation/focus-on-bison/CT_Index\" id=\"anch_84\">Bison from Farm to Table</a> (USDA)<br>Safe storage, handling, and preparation of bison.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Corned Beef",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…-safety-fact-sheets/meat-preparation/corned-beef-and-food-safety/CT_Index\" id=\"anch_85\">Corned Beef and Food Safety</a> (USDA)<br>Safe preparation, cooking, and storage of corned beef.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Game",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…food-safety-fact-sheets/meat-preparation/game-from-farm-to-table/CT_Index\" id=\"anch_86\">Food Safety of Farm-Raised Game</a> (USDA)<br>Learn about safe handling of venison, antelope, boar, and other game.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Goat",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…food-safety-fact-sheets/meat-preparation/goat-from-farm-to-table/CT_Index\" id=\"anch_87\">Goat from Farm to Table</a> (USDA)<br>Safe storage, handling, and preparation of goat.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Ham",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ers/food-safety-fact-sheets/meat-preparation/ham-and-food-safety/CT_Index\" id=\"anch_88\">Ham and Food Safety</a> (USDA)<br>Includes charts on cooking and storing ham.</p><p><a href=\"/keep/charts/hamcookingchart.html\" title=\"\" id=\"anch_89\">Ham Cooking Chart</a></p><p><a href=\"/keep/charts/hamstoragechart.html\" title=\"\" id=\"anch_90\">Ham Storage Chart</a></p>"
+    }, {
+        "type": "Meat",
+        "name": "Hamburger (Ground Beef)",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…-safety-fact-sheets/meat-preparation/ground-beef-and-food-safety/CT_Index\" id=\"anch_91\">Ground Beef</a> (USDA)<br>The most frequently asked questions about ground beef.</p><p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…reparation/color-of-cooked-ground-beef-as-it-relates-to-doneness/CT_Index\" id=\"anch_92\">Color of Cooked Ground Beef as It Relates to Doneness</a> (USDA)<br>Is color a good indicator of doneness? Not always. Use a meat thermometer to make sure that ground beef is cooked to 160 °F.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Hot Dogs",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ood-safety-fact-sheets/meat-preparation/hot-dogs-and-food-safety/CT_Index\" id=\"anch_93\">Hot Dogs and Food Safety</a> (USDA)<br>Find out what’s in a hot dog and how to keep them safe.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Jerky",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…s/food-safety-fact-sheets/meat-preparation/jerky-and-food-safety/ct_index\" id=\"anch_94\">Food Safety of Jerky</a> (USDA)<br>The safest procedure to follow when making homemade jerky.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Lamb",
+        "description": "<a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ety-fact-sheets/meat-preparation/focus-on-lambfrom-farm-to-table/CT_Index\" id=\"anch_95\">Lamb from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods, and approximate cooking times for lamb."
+    }, {
+        "type": "Meat",
+        "name": "Pork",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…afety-fact-sheets/meat-preparation/fresh-pork-from-farm-to-table/CT_Index\" id=\"anch_96\">Pork from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods, and approximate cooking time for pork.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Rabbit",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…od-safety-fact-sheets/meat-preparation/rabbit-from-farm-to-table/CT_Index\" id=\"anch_97\">Rabbit from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods, and approximate cooking times for rabbit.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Sausages",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ood-safety-fact-sheets/meat-preparation/sausages-and-food-safety/CT_Index\" id=\"anch_98\">Sausages and Food Safety</a> (USDA)<br>Includes a storage chart on proper handling.</p>"
+    }, {
+        "type": "Meat",
+        "name": "Veal",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…food-safety-fact-sheets/meat-preparation/veal-from-farm-to-table/CT_Index\" id=\"anch_99\">Veal from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods, and approximate cooking time for veal.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Chicken",
+        "description": "<a href=\"http://www.fsis.usda.gov/wps/wcm/connect/ad74bb8d-1dab-49c1-b05e-390a74ba7471/Chicken_from_Farm_to_Table.pdf?MOD=AJPERES\" id=\"anch_86\">Chicken From Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods and times for chicken."
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Duck",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…fact-sheets/poultry-preparation/duck-and-goosefrom-farm-to-table/CT_Index\" id=\"anch_87\">Duck and Goose from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods, and cooking times for duck.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Emu",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ety-fact-sheets/poultry-preparation/ratites-emu-ostrich-and-rhea/CT_Index\" id=\"anch_88\">Ratites (Emu, Ostrich, and Rhea)</a> (USDA)<br>Safe storage, handling, and cooking methods for these birds.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Game Birds",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…food-safety-fact-sheets/meat-preparation/game-from-farm-to-table/CT_Index\" id=\"anch_89\">Food Safety of Farm-Raised Game</a> (USDA)<br>Learn about safe handling of partridge, quail, pheasant, and other game birds.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Giblets",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…d-safety-fact-sheets/poultry-preparation/giblets-and-food-safety/CT_Index\" id=\"anch_90\">Giblets and Food Safety</a> (USDA)<br>Inspection, processing, safe handling, and cooking of poultry giblets.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Goose",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…fact-sheets/poultry-preparation/duck-and-goosefrom-farm-to-table/CT_Index\" id=\"anch_91\">Duck and Goose from Farm to Table</a> (USDA)<br>Safe storage, handling, cooking methods and times for goose.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Ground Poultry",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…y-fact-sheets/poultry-preparation/ground-poultry-and-food-safety/CT_Index\" id=\"anch_92\">Ground Poultry and Food Safety</a> (USDA)<br>Labeling, storage, and cooking of ground poultry.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Ostrich",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ety-fact-sheets/poultry-preparation/ratites-emu-ostrich-and-rhea/CT_Index\" id=\"anch_93\">Ratites (Emu, Ostrich, and Rhea)</a> (USDA)<br>Safe storage, handling, and cooking methods for these birds.</p>"
+    }, {
+        "type": "Chicken and Other Poultry",
+        "name": "Rhea",
+        "description": "<p><a href=\"http://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-a…ety-fact-sheets/poultry-preparation/ratites-emu-ostrich-and-rhea/CT_Index\" id=\"anch_94\">Ratites (Emu, Ostrich, and Rhea)</a> (USDA)<br>Safe storage, handling, and cooking methods for these birds.</p>"
+    }]
+};
